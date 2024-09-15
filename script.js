04ffedc121cd4ad2dfc8f0df1c5f9acfdde6cc6e
Muhammad Yousuf <imhere4uinfo@gmail.com>
2024-09-16 04:04:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-// var display = document.getElementById("display");
-function appendToDisplay(input) {
-    display.value += input;
-};
-
-function clearDisplay() {
-    display.value = "";
-};
-
+// Get  display element
+var display = document.getElementById("display");
+// Function to append values to display
+function appendToDisplay(value) {
+    // Check if the current display value length is less than 9
+    if (display.value.length < 9) {
+        display.value += value;
+    }
+    else {
+        alert("Maximum of 9 digits allowed");
+    }
+}
+// Function to calculate the result
 function calculate() {
     try {
+        // Evaluate the expression in the display
         display.value = eval(display.value);
     }
     catch (error) {
         display.value = "Error";
-    };
-};
+    }
+}
+// Function to clear the display
+function clearDisplay() {
+    display.value = "";
+}
